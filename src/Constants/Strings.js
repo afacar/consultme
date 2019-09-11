9390978a596f7f39fc9751206e4beb559470e4a9
JavidHaji-zada <JavidHaji-zada@users.noreply.github.com>
2019-09-11 10:43:44 +0300

    AGREEMENT_POLICY added into strings

--- src/Constants/Strings.js
+++ src/Constants/Strings.js
@@ -1,6 +1,7 @@
 const strings = {
     DEFAULT_PROFILE_PIC:"https://firebasestorage.googleapis.com/v0/b/consultme-cb5ad.appspot.com/o/profilePics%2Fdefault_profile_pic.jpg?alt=media&token=a2795508-3827-4712-ab1f-556bfd8b920d",
     APP_NAME:"Consult Me",
-    APP_NAME_CAPITAL:"CONSULT ME"
+    APP_NAME_CAPITAL:"CONSULT ME",
+    AGREEMENT_POLICY: 'DUMMY ANTLAŞMA METNİ'
 };
 export default strings;
